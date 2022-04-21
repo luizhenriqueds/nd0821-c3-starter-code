e776a6b9b86cfdd63990b533b8bf2e2695735d14
Luiz Henrique <luizhsda@gmail.com>
2022-04-21 10:17:48 -0300

    Added ml training code

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,4 +1,15 @@
+import logging
+
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+
+from starter.starter.ml.data import process_data
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)-15s %(message)s"
+)
+logger = logging.getLogger()
 
 
 # Optional: implement hyperparameter tuning.
@@ -18,7 +29,17 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    # Train and save a model.
+    rfc = RandomForestClassifier(
+        n_estimators=150,
+        max_depth=10,
+        random_state=42
+    )
+
+    logger.info(f"{__name__} - Training model")
+    rfc.fit(X_train, y_train)
+
+    return rfc
 
 
 def compute_model_metrics(y, preds):
@@ -37,10 +58,68 @@ def compute_model_metrics(y, preds):
     recall : float
     fbeta : float
     """
+    logger.info(f"{__name__} - Computing model metrics")
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
     recall = recall_score(y, preds, zero_division=1)
     return precision, recall, fbeta
+
+
+def compute_slice_metrics(data, model, encoder, lb, categorical_features):
+    """
+    Validates the trained machine learning model on slices of
+    the data using precision, recall, and F1.
+
+    Inputs
+    ------
+    reporter : pd.Dataframe
+        Test data to be used in slicing.
+    model : RandomForestClassifier
+        Trained classifier to perform predictions.
+    encoder: sklearn.preprocessing._encoders.OneHotEncoder
+        Fitted encoder.
+    lb: sklearn.preprocessing._label.LabelBinarizer
+        Fitted binarizer.
+    categorical_features: list
+        Categorical features to compute slices.
+    Returns
+    -------
+    reporter : dict
+        A metric reporter with the slice metrics for all
+    categorical features
+    """
+    logger.info(f"{__name__} - Computing model metrics on slice")
+
+    reporter = []
+    for col in categorical_features:
+        cat_values = data[col].unique()
+        for value in cat_values:
+            logger.info(f"\n{__name__} Measure performance on feature {col}")
+            filtered_test = data[data[col] == value]
+            X_test, y_test, _, _ = process_data(
+                filtered_test,
+                categorical_features=categorical_features,
+                label="salary",
+                training=False,
+                encoder=encoder,
+                lb=lb
+            )
+
+            preds = inference(model, X_test)
+            precision, recall, fbeta = compute_model_metrics(
+                y_test, preds
+            )
+            metrics = {
+                "feature": col,
+                "value": value,
+                "metrics": {
+                    "precision": round(precision, 5),
+                    "recall": round(recall, 5),
+                    "fbeta": round(fbeta, 5)
+                }
+            }
+            reporter.append(metrics)
+    return reporter
 
 
 def inference(model, X):
@@ -48,7 +127,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : RandomForestClassifier
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +136,5 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    logger.info(f"{__name__} - Computing model predictions")
+    return model.predict(X)
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,17 @@
 # Script to train machine learning model.
 
+import json
+
+import joblib
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
+from ml import model
 # Add the necessary imports for the starter code.
+from ml.data import process_data
 
 # Add code to load in the data.
+data = pd.read_csv("../data/clean_census_data.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -24,5 +31,38 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder_test, lb_test = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
 
-# Train and save a model.
+joblib.dump(encoder, "../model/encoder.pkl")
+
+# Training model
+clf = model.train_model(X_train, y_train)
+joblib.dump(clf, "../model/rfc_model.pkl")
+
+# Computing predictions
+preds = model.inference(clf, X_test)
+
+precision, recall, fbeta = model.compute_model_metrics(
+    y_test, preds
+)
+
+print(
+    f"model metrics: \n"
+    f"\tprecision: {precision} \n"
+    f"\trecall: {recall} \n"
+    f"\tfbeta: {fbeta}"
+)
+
+reporter = model.compute_slice_metrics(
+    test, clf, encoder, lb, cat_features
+)
+
+with open("../data/slice_output.txt", 'w') as file:
+    file.write(json.dumps(reporter, indent=4))
